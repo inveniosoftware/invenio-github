b1cf655ac4eb37cf7d22ee4eafd0abc7ea4901fb
Georgios Lignos <glignos93@gmail.com>
2019-07-02 11:02:44 +0200

    setup: fix pytest version to be compatible with pytest-cov

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ tests_require = [
     'pytest-cache>=1.0',
     'pytest-cov>=1.8.0',
     'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
+    'pytest>=3.6.0',  # due to pytest-cov requirement
 ]
 
 extras_require = {
