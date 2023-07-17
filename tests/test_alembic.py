5e59d445a960b636576b2ccbe677056d4f79d5e6
alejandromumo <manuel.alejandro.de.oliveira.da.costa@cern.ch>
2023-07-17 13:30:40 +0100

    alembic: added upgrade recipes

---
+++ tests/test_alembic.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2023 CERN.
+#
+# Invenio-Github is free software; you can redistribute it and/or modify
+# it under the terms of the MIT License; see LICENSE file for more details.
+"""Test invenio-github alembic."""
+
+import pytest
+from invenio_db.utils import alembic_test_context, drop_alembic_version_table
+
+
+def test_alembic(base_app, database):
+    """Test alembic recipes."""
+    db = database
+    ext = base_app.extensions["invenio-db"]
+
+    if db.engine.name == "sqlite":
+        raise pytest.skip("Upgrades are not supported on SQLite.")
+
+    base_app.config["ALEMBIC_CONTEXT"] = alembic_test_context()
+
+    # Check that this package's SQLAlchemy models have been properly registered
+    tables = [x.name for x in db.get_tables_for_bind()]
+    assert "github_repositories" in tables
+    assert "github_releases" in tables
+
+    # Check that Alembic agrees that there's no further tables to create.
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    # Drop everything and recreate tables all with Alembic
+    db.drop_all()
+    drop_alembic_version_table()
+    ext.alembic.upgrade()
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    # Try to upgrade and downgrade
+    ext.alembic.stamp()
+    ext.alembic.downgrade(target="96e796392533")
+    ext.alembic.upgrade()
+    assert len(ext.alembic.compare_metadata()) == 0
+
+    drop_alembic_version_table()
